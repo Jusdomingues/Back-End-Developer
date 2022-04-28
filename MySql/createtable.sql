c88d1044f253f2668f9a3a8a60951ade2a2e7207
Jusdomingues <julianasdomingues02@gmail.com>
2022-04-28 00:18:46 -0300

    Criando tabela no mysql

--- MySql/createtable.sql
+++ MySql/createtable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE cli_clientes
 (
 	-- not null indicando como obrigatório
-	cli_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	-- cli_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cli_id SERIAL PRIMARY KEY, -- BIGINT NOT NULL AUTO_INCREMENT UNSIGNED UNIQUE
 	cli_nome VARCHAR(50) NOT NULL,
     cli_data_nascimento DATE,
     cli_logradouro VARCHAR(50)
